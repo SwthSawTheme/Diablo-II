7fdeb84430766595e17b8b5af9896f2c2d41d6aa
SwthSawTheme <sawtheme1@gmail.com>
2025-05-11 12:52:40 -0300

    Sistema de login criado com sucesso

--- app/models/login.py
+++ app/models/login.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class User():
 
     def __init__(self, login:str,password:str):
@@ -11,9 +13,24 @@ class User():
 
         return insert, (self.login, self.password)
 
-    def login(self):
-        pass
+    def logar(self):
+        try:
+            conexao = sqlite3.connect("Data.db")
+            cursor = conexao.cursor()
+            cursor.execute("SELECT * FROM USERS WHERE Login = ? AND password = ?",(self.login,self.password))
+            if cursor.fetchone():
+                print("Logado com sucesso!")
+            else:
+                print("Login ou senha incorretos!")
+        except Exception as e:
+            print(f"Error{e}")
+
 
     def getUsers(self):
         pass
-        
+
+if __name__ == "__main__":
+    user = str(input("Usuario: "))
+    password = str(input("Senha: "))
+    usuario = User(user,password)
+    usuario.logar()
